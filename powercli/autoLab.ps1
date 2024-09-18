3895306afbff34e6c4be63abd175543ba7457917
benhtodd <benhtodd@gmail.com>
2024-09-18 23:46:41 +0000

    rdfewrfwe

--- powercli/autoLab.ps1
+++ powercli/autoLab.ps1
@@ -8,20 +8,24 @@
 # ====================================================================================
 
 # ==========  Tanzu Namespace Settings ===============================================
-$wmNamespaceName = "lf-tdd-namespace-3"
-$spbmPolicyName = "vsan-tanzu-storage"
+# $wmNamespaceName = "lf-test-tdd"
+$wmNamespaceName = "lf-tdd-namespace"
 $domainFqdn = "vcf.holo.lab"
 $domainBindUser = "svc-vsphere-ad"
 $domainBindPass = "VMware123!"
-
 $wmNamespaceEditUserGroup = "gg-kub-admins"
 $wmNamespaceViewUserGroup = "gg-kub-readonly"
-
-$vmClass = "guaranteed-small"
+# vSAN Datastore Tag and Storage Policy Settings
+$tagCategoryName = "vsphere-with-tanzu-category"
+$tagName = "vsphere-with-tanzu-tag"
+$spbmPolicyName = "vsan-tanzu-storage"
+$vmClassSmall = "best-effort-small"
+$vmClassMedium = "best-effort-medium"
 # ==========  SDDC Manager Info ======================================================
 $sddcManagerFqdn = "sddc-manager.vcf.sddc.lab"
 $sddcManagerUser = "administrator@vsphere.local"
-$sddcManagerPass = "VMware123!"
+$sddcManagerPass = "VMware123!VMware123!"
+$sddcDomainName = "mgmt-domain"
 # ==========  vCenter Info   =========================================================
 $vcFQDNName = "vcenter-mgmt.vcf.sddc.lab"
 $vcUserName ='administrator@vsphere.local'
@@ -29,54 +33,52 @@ $vcPassword = 'VMware123!VMware123!'
 $vcFQDNName = "vcenter-mgmt.vcf.sddc.lab"
 $superVisorName = "lf-tdd-supervisor"
 $iassCplaneCluster = "mgmt-cluster-01"
-$sddcManagerFQDN = ""
 # ====================================================================================
 Connect-VIServer -Server $vcFQDNName -Protocol https -User $vcUserName -Password $vcPassword
 
 # ====================================================================================
 #  Crate Tag Category and Tag
+New-TagCategory -Name $tagCategoryName -Description "lf-dev.ready.tdd.lab" -Cardinality single -EntityType Datastore
+New-Tag -Name $tagName -Category $tagCategoryName
 # ====================================================================================
-New-TagCategory -Name vsphere-with-tanzu-category-2 -Description lf-dev.ready.tdd.lab -Cardinality single -EntityType Datastore
+#  Assign Tag to Datstore
+New-TagAssignment -Tag $tagName -Entity "vcf-vsan"
+# ====================================================================================
+#  Crate Tag Category and Tag
+# Set-DatastoreTag -Server $sddcManagerFqdn -User $sddcManagerUser -Pass $sddcManagerPass -Domain $sddcDomainName -TagName $tagName -TagCategoryName $tagCategoryName
 
-New-Tag -Name “vsphere-with-tanzu-tag-2” -Category “vsphere-with-tanzu-category-2”
+# ====================================================================================
+# Create new vSAN Storage Policy for Tanzu associated with Storage Tag
+Add-StoragePolicy -Server $sddcManagerFqdn -User $sddcManagerUser -Pass $sddcManagerPass -Domain $sddcDomainName -PolicyName $spbmPolicyName -TagName $tagName
 
-New-TagAssignment -Tag "vsphere-with-tanzu-tag-2" -Entity "vcf-vsan"
 
 
 # ====================================================================================
 #  Create and Configure Namespace
 # ====================================================================================
-$newNameSpace = "lf-test-tdd" # NOTE: Change this to match new Namespace 
 $userDomain1 = "vsphere.local"
 $userDomain2 = "vcf.holo.lab"
-$userDomain3 = ""
-
 # ====================================================================================
 #  Create Namespace
-New-WMNamespace -Name $newNameSpace -Cluster $iassCplaneCluster
-Set-WMNamespace $newNameSpace -Description "LF TDD NameSpace For Lab"
+New-WMNamespace -Name $wmNamespaceName -Cluster $iassCplaneCluster
+Set-WMNamespace $wmNamespaceName -Description "LF TDD NameSpace For Lab"
 
 # ====================================================================================
 #  Create Namespace permisions
 #  Create Owner
-# New-WMNamespacePermission -Domain $userDomain1 -Namespace $newNameSpace -PrincipalName "kk-kub-admins" -PrincipalType Group -Role (Get-VIRole -Name 'Owner')
-New-WMNamespacePermission -Domain $userDomain1 -Namespace $newNameSpace -PrincipalName "gg-kub-admins" -PrincipalType Group -Role 'Admin'
+# New-WMNamespacePermission -Domain $userDomain1 -Namespace $wmNamespaceName -PrincipalName "kk-kub-admins" -PrincipalType Group -Role (Get-VIRole -Name 'Owner')
+New-WMNamespacePermission -Domain $userDomain1 -Namespace $wmNamespaceName -PrincipalName "gg-kub-admins" -PrincipalType Group -Role 'Admin'
 #  Create Edit
-New-WMNamespacePermission -Domain $userDomain2 -Namespace $newNameSpace -PrincipalName "gg-kub-devs" -PrincipalType Group -Role 'Edit'
+New-WMNamespacePermission -Domain $userDomain2 -Namespace $wmNamespaceName -PrincipalName "gg-kub-devs" -PrincipalType Group -Role 'Edit'
 #  Create View
-New-WMNamespacePermission -Domain $userDomain2 -Namespace $newNameSpace -PrincipalName "gg-kub-readonly" -PrincipalType Group -Role'View'
+New-WMNamespacePermission -Domain $userDomain2 -Namespace $wmNamespaceName -PrincipalName "gg-kub-readonly" -PrincipalType Group -Role'View'
 
 # ====================================================================================
-#  Create Namespace
 #  Add Storage Policy
-New-WMNamespaceStoragePolicy -Namespace $newNameSpace -StoragePolicy vsan-tanzu-storage
+New-WMNamespaceStoragePolicy -Namespace $wmNamespaceName -StoragePolicy "vsan-tanzu-storage"
 
 
-
-
-Add-NamespaceVmClass -server $ -user administrator@vsphere.local -pass VMw@re1! -domain sfo-w01 -namespace sfo-w01-tkc01 -vmClass guaranteed-small
-
-
-
-
-Get-WMNamespace -Name "lf-tdd-namespace"
+# ====================================================================================
+#  Add VMClass
+Add-NamespaceVmClass -server $sddcManagerFqdn -user $sddcManagerUser -pass $sddcManagerPass -domain $sddcDomainName -Namespace $wmNamespaceName -VMClass $vmClassSmall
+Add-NamespaceVmClass -server $sddcManagerFqdn -user $sddcManagerUser -pass $sddcManagerPass -domain $sddcDomainName -Namespace $wmNamespaceName -VMClass $vmClassMedium
